f766e1d75628d69169c3043e39910f52e4ffc236
govtbodies <tom.ignarri@gmail.com>
2021-01-16 13:15:10 -0500

    map tiling seems to be working

--- src/Components/MapView.js
+++ src/Components/MapView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import '../App.css';
 import ReactDOM from 'react-dom';
 
 
@@ -13,12 +14,19 @@ class MapView extends React.Component{
       }
     }
 
+    
     render(){
         return(
 
-
+            
             <div>
-               <MapContainer center={this.state.currentLocation} zoom={this.state.zoom} style={{height: 350}}></MapContainer>
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+               <MapContainer center={this.state.currentLocation} zoom={this.state.zoom} style={{height: 350}}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+               </MapContainer>
             </div>
 
 
