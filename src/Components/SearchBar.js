6e69b6a5c952e87d073795b03bc7758b5cf0324c
govtbodies <tom.ignarri@gmail.com>
2021-01-25 16:12:27 -0500

    some fetch setup

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,6 +4,23 @@ import ReactDOM from 'react-dom';
 
 
 class SearchBar extends React.Component{
+
+    onSearchSubmit = () => {
+        const headers = {
+            'apiKey': 'at_SGVjuwbf6VmSgiEm49I9lxFY1Q0FY',
+            'ipAddress': '8.8.8.8',
+        }
+
+        fetch('https://geo.ipify.org/api/v1?', { headers })
+        .then(async response => {
+            const data = await response.json();
+            console.log(data);
+        })
+        .catch(error => {
+
+        });
+    }
+
     render(){
         return(
             <div class="searchBar">
