23f5b15284394f9330f3526cc5638f4e05cca056
govtbodies <tom.ignarri@gmail.com>
2021-03-02 14:03:55 -0500

    fixed info passing in useContext

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -6,13 +6,13 @@ import { ipAddressContext } from '../App'
 
 function Display(props){
 
-    const state = useContext(ipAddressContext);
+    const {state, dispatch} = useContext(ipAddressContext);
     
 
 
     return(
         <div id="searchDisplay">
-            <div className="displayBox">{"country" || state.locationData.country}</div>
+            <div className="displayBox">{state.locationData.country}</div>
             <div className="displayBox"></div>
             <div className="displayBox"></div>
             <div className="displayBox"></div>
