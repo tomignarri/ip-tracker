5160c9f1cd9b18421ab430f6551ee8cac43d578e
govtbodies <tom.ignarri@gmail.com>
2021-05-12 16:29:00 -0400

    restructured location service import/export

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,59 +3,62 @@ import '../App.css';
 
 import { ipAddressContext } from '../App'
 import LoadingIcon from "./LoadingIcon";
+import LocationService, {onSearchSubmit, isLoading} from "../Services/LocationService"
 
 
 function SearchBar(){
 
-    const {state, dispatch} = useContext(ipAddressContext);
 
-    const [isLoading, setIsLoading] = useState(false); 
+    
+    //const {state, dispatch} = useContext(ipAddressContext);
 
-    const isValidIp = (ipAddressQuery) =>{
-        var splitIp = ipAddressQuery.split(".");
-        console.log("SPLIT IP  ", splitIp);
+    //const [isLoading, setIsLoading] = useState(false); 
+
+    // const isValidIp = (ipAddressQuery) =>{
+    //     var splitIp = ipAddressQuery.split(".");
+    //     console.log("SPLIT IP  ", splitIp);
         
-        if(splitIp.length !== 4){
-            return false;
-        }
+    //     if(splitIp.length !== 4){
+    //         return false;
+    //     }
 
-        for(var i = 0; i < splitIp.length; i++){
-            if(splitIp[i] > 255 || splitIp[i] < 0 || isNaN(splitIp[i])){
-                return false;
-            }
-        };
-        return true;
-    }
+    //     for(var i = 0; i < splitIp.length; i++){
+    //         if(splitIp[i] > 255 || splitIp[i] < 0 || isNaN(splitIp[i])){
+    //             return false;
+    //         }
+    //     };
+    //     return true;
+    // }
     
 
-    const onSearchSubmit = (ipAddressQuery) => {
-        console.log("SUBMITING", ipAddressQuery);
+    // const onSearchSubmit = (ipAddressQuery) => {
+    //     console.log("SUBMITING", ipAddressQuery);
         
-        if(!isValidIp(ipAddressQuery)){
-            alert("invalid ip address");
-            return;
-        }
+    //     if(!isValidIp(ipAddressQuery)){
+    //         alert("invalid ip address");
+    //         return;
+    //     }
 
-        setIsLoading(true);
+    //     setIsLoading(true);
 
-        const apiKey = 'at_SGVjuwbf6VmSgiEm49I9lxFY1Q0FY';
+    //     const apiKey = 'at_SGVjuwbf6VmSgiEm49I9lxFY1Q0FY';
         
-        fetch('https://geo.ipify.org/api/v1?apiKey=' + apiKey + '&ipAddress=' + ipAddressQuery,{
+    //     fetch('https://geo.ipify.org/api/v1?apiKey=' + apiKey + '&ipAddress=' + ipAddressQuery,{
             
-        })
-        .then(async response => {
-            const data = await response.json();
-            dispatch({ type: 'UPDATE_LOCATION', data: data});
-            setIsLoading(false);
-            console.log("data!!!", data);
-            console.log("loading!!!", isLoading);
-           
-        })
-        .catch(error => {
-            console.log(error)
-        });
+    //     })
+    //     .then(async response => {
+    //         const data = await response.json();
+    //         dispatch({ type: 'UPDATE_LOCATION', data: data});
+    //         setIsLoading(false);
+    //         console.log("data!!!", data);
+    //         console.log("loading!!!", isLoading);
+    //         
+    //     })
+    //     .catch(error => {
+    //         console.log(error)
+    //     });
         
-    }
+    // }
 
     return(
         <div className="searchBar">
